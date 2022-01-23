662e20a4e4066b2a5114c0df61cfd5586945d027
chriskmil <93924532+chriskmil@users.noreply.github.com>
2022-01-23 18:50:22 -0500

    app page update

---
+++ js/site.js
@@ -0,0 +1,24 @@
+// get th var entered values
+function getValues() {
+    let loanamount
+    let interest
+    let term
+
+}
+
+// generate an array of payment objects
+function generatePAyments() {
+
+    let paymentsArray = [];
+
+    let paymentobj = [];
+
+    paymentsArray.push(paymentsArray);
+
+}
+
+//display an array of payment objects
+function displayPayments(paymentsArray) {
+
+
+}
